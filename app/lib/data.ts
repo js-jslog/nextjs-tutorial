6f28bb6f07c748a906624005b8e5bfeeafd610a2
js-jslog <joseph.sinfield@yahoo.com>
2024-03-04 13:51:04 +0000

    Replacing the experimental function with standard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,11 +9,8 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  noStore();
-
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -33,8 +30,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
-
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -55,8 +50,6 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore();
-
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -96,8 +89,6 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
-  noStore();
-
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -130,8 +121,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
-
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -153,8 +142,6 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -198,8 +185,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
-
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
